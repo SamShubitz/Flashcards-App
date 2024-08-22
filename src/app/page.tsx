f96637f4766ca19e9138a26a90a72690e84e9c05
Sam Shubitz <shubitzs@gmail.com>
2024-08-22 12:59:58 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { getUser } from "@/lib/get-user";
 
 export default async function Home() {
-  const user = await getUser();
+  const response = await getUser();
+  const user = response?.user;
 
   return (
     <main className="bg-slate-900 flex justify-center items-center p-24 gap-9">
       <div className="flex-1 max-w-96 text-left text-white">
-        <h1 className="text-4xl">Welcome</h1>
+        <h1 className="text-4xl">Welcome {user && user?.username}</h1>
         <br />
         <p>
-          {`This is a 'toy app' that allows users to save and customize flashcards decks, or play a simple trivia game.`}
+          {`This is a web app that allows users to save and customize flashcard decks or play a simple trivia game.`}
         </p>
       </div>
     </main>
